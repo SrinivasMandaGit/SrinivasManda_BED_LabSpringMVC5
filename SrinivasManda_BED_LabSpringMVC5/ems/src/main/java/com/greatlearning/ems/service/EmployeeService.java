42f390674dbfd9e307c291bfa5dc3a3b035bef4d
SrinivasManda <105982465+SrinivasMandaGit@users.noreply.github.com>
2024-04-08 15:06:32 +0530

    Add files via upload
    GreatLearning_Lab5_SpringMVC_Assignment

---
+++ SrinivasManda_BED_LabSpringMVC5/ems/src/main/java/com/greatlearning/ems/service/EmployeeService.java
@@ -0,0 +1,17 @@
+package com.greatlearning.ems.service;
+
+import java.util.List;
+
+import com.greatlearning.ems.model.Employee;
+
+public interface EmployeeService {
+
+	public List<Employee> findAll();
+
+	public void save(Employee employee);
+
+	public Employee findById(Integer employeeID);
+
+	public void deleteById(Integer employeeId);
+
+}
